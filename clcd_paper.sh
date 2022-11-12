be478e642101ef3d162002f77dd16042565a14d7
Ivsucram <ivsucram@gmail.com>
2022-11-12 12:37:01 +0800

    Fix results scripts

--- clcd_paper.sh
+++ clcd_paper.sh
@@ -58,6 +58,7 @@ do
 done
 
 for SEED in 0 1 2 3 4 5 6 7 8 9
+do
     python train.py --config 'configs/datasets/usps_mnist.yml' --experiment 'usps_mnist_ablation_A' --seed=$SEED --alpha-1=0 --log-wandb
     python train.py --config 'configs/datasets/mnist_usps.yml' --experiment 'mnist_usps_ablation_A' --seed=$SEED  --alpha-1=0 --log-wandb
     python train.py --config 'configs/datasets/visda.yml' --experiment 'visda_ablation_A' --seed=$SEED --alpha-1=0 --log-wandb
